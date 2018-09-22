2d4eb13f276d3e41af84e6e3312fcf2d1d1d3ab3
FriendlyUser <studentdavidli@gmail.com>
2018-09-22 10:04:05 -0700

    life is never that simple

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Router, Route } from 'react-router'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Router> <App /> </Router>, div);
 });
