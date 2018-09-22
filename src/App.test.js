36171564fa1ed914978c8a05a83d559e2a662454
FriendlyUser <studentdavidli@gmail.com>
2018-09-22 11:56:42 -0700

    adding files

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Router, Route } from 'react-router'
 
-import { history, store } from './store'
 it('renders without crashing', () => {
-    ReactDOM.render(
-    <Router history={history} store={store}> <Route exact path="/" component={App} /> </Router>, document.getElementById('root'));
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
 });
