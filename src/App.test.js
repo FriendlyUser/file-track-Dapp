aa0434b7b89629c157445d64d58b8af628c21567
FriendlyUser <studentdavidli@gmail.com>
2018-09-22 11:34:42 -0700

    hello

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,6 @@ import { Router, Route } from 'react-router'
 
 import { history, store } from './store'
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <Router history={history} store={store}> <Route exact path="/" component={App} /> </Router>, div);
+    ReactDOM.render(
+    <Router history={history} store={store}> <Route exact path="/" component={App} /> </Router>, document.getElementById('root'));
 });
