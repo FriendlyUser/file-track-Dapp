0e94bd54d75ca914e781fbc5ed0ff8968751f52f
FriendlyUser <studentdavidli@gmail.com>
2018-09-22 10:22:03 -0700

    Updating pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Router, Route } from 'react-router'
 
+import { history, store } from './store'
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
