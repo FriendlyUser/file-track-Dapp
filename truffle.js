9c49e327fcea80dba56d486be6284627704f4cee
FriendlyUser <studentdavidli@gmail.com>
2018-09-28 20:48:39 -0700

    fucking derp

--- truffle.js
+++ truffle.js
@@ -31,7 +31,7 @@ module.exports = {
     coverage: {
       host: "localhost",
       network_id: "*",
-      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+      port: 9545,         // <-- If you change this, also set the port option in .solcover.js.
       gas: 0xfffffffffff, // <-- Use this high gas value
       gasPrice: 0x01      // <-- Use this low gas price
     },
