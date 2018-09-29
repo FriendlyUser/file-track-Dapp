534597caf4712c0fe4f1f83be5ab101fcbe6ff21
FriendlyUser <studentdavidli@gmail.com>
2018-09-28 20:11:28 -0700

    fixing errors

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = { 
-    compileCommand: '../node_modules/.bin/truffle compile',
-    testCommand: 'node --max-old-space-size=8192 ../node_modules/.bin/truffle test --network coverage',
+    port: 9545,
+    testrpcOptions: '-p 9545 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"',
     copyPackages: ['openzeppelin-solidity']
 };
