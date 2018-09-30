9045278659509bf80c474239540b43660b388163
FriendlyUser <studentdavidli@gmail.com>
2018-09-30 14:18:40 -0700

    updating package.json and fixing build|

--- src/drizzleOptions.js
+++ src/drizzleOptions.js
@@ -1,7 +1,7 @@
 import ComplexStorage from './../build/contracts/ComplexStorage.json'
 import SimpleStorage from './../build/contracts/SimpleStorage.json'
 import TutorialToken from './../build/contracts/TutorialToken.json'
-import FileList from './../build/contracts/FileList.json'
+// import FileList from './../build/contracts/FileList.json'
 
 const drizzleOptions = {
   web3: {
