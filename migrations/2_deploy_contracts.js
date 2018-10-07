369e3842c6d86408d2f1294fa4eb6828e6937657
FriendlyUser <studentdavidli@gmail.com>
2018-10-07 10:59:06 -0700

    goofed up spelling

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ var TutorialToken = artifacts.require("TutorialToken");
 var ComplexStorage = artifacts.require("ComplexStorage");
 var FileList = artifacts.require("FileList")
 var Authentication = artifacts.require("Authentication")
-var Auth2 = artifacts.required("Authentication")
+var Auth2 = artifacts.require("Authentication")
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(TutorialToken);
