54d5872d5cc82856f11d63a7fc2777d8951c829d
FriendlyUser <studentdavidli@gmail.com>
2018-10-07 10:46:41 -0700

    adding testing

---
+++ test/AuthenticationTest.js
@@ -0,0 +1,25 @@
+var Authentication = artifacts.require("./Authentication.sol");
+
+contract('Authentication', function(accounts) {
+
+  it("...should sign up and log in a user.", function() {
+    return Authentication.deployed().then(function(instance) {
+      authenticationInstance = instance;
+
+      return authenticationInstance.signup('testuser', {from: accounts[0]});
+    }).then(function() {
+      return authenticationInstance.login.call();
+    }).then(function(userName) {
+      assert.equal(web3.toUtf8(userName), 'testuser', "The user was not signed up.");
+    });
+  });
+  it("...should destroy the existing user.", function() {
+    return Authentication.deployed().then(function(instance) {
+      authenticationInstance = instance
+      
+      return authenticationInstance.destroy.call();
+    }).then(function() {
+      // assert.equal(web3.toUtf8(userName), 'testuser', "The user was not signed up.");
+    });
+  });
+});
